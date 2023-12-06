e2846f61827b1779d122b266da351f074dd328d4
Herbert Ntim <herbertntim15@gmail.com>
2023-12-06 01:14:10 +0000

    working on file upload

--- src/_root/pages/CreatePost.tsx
+++ src/_root/pages/CreatePost.tsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import PostForm from "@/components/forms/PostForm"
 
 const CreatePost = () => {
   return (
-    <div>CreatePost</div>
+    <section className="flex flex-1">
+      <div className="common-container">
+        <div className="max-w-5xl flex-start justify-start gap-3 w-full"> 
+          <img 
+            src="assets/icons/add-post.svg" 
+            alt="post"
+            width={36}
+            height={36} 
+          />
+          <h2 className="h3-bold md:h2-bold w-full text-left">Create Post</h2>
+        </div>
+
+        <PostForm />
+      </div>
+    </section>
   )
 }
 
