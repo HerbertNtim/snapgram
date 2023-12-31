8d6085a1350f24969cf86dd11d939a62a7c15729
Herbert Ntim <herbertntim15@gmail.com>
2023-12-31 20:04:54 +0000

    can add more ideas

--- src/components/forms/PostForm.tsx
+++ src/components/forms/PostForm.tsx
@@ -142,7 +142,12 @@ const PostForm = ({ post, action }: PostFormProps) => {
         />
 
         <div className="flex gap-4 justify-end items-center">
-          <Button type="button"  className="shad-button_dark_4">Cancel</Button>
+          <Button 
+            type="button"  className="shad-button_dark_4"
+            onClick={() => navigate(-1)}
+          >
+            Cancel
+          </Button>
           <Button
             type="submit"
             className="shad-button_primary whitespace-nowrap"
